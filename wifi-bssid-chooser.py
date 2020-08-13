652f810bc5468103b97e37e6d59ec1d7a138d268
gnzng <65827185+gnzng@users.noreply.github.com>
2020-08-13 20:08:39 +0200

    Create wifi-bssid-chooser.py

---
+++ wifi-bssid-chooser.py
@@ -0,0 +1,41 @@
+### code by gnzng
+
+import objc, os, time, subprocess
+
+process = subprocess.Popen(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport','-s'], stdout=subprocess.PIPE)
+out, err = process.communicate()
+process.wait()
+print(out)
+
+print('make sure it is the only network your mac is able to connect to\n\n')
+
+aim =  str(raw_input('enter bssid to connect to in roaming network:\n')).lower()
+
+if aim == '':
+    aim =  '94:8f:cf:93:a6:fc'.lower()    #for often used connection
+    print('try to connect to {}'.format(aim))
+
+def connect():
+    objc.loadBundle('CoreWLAN',
+                    bundle_path = '/System/Library/Frameworks/CoreWLAN.framework',
+                    module_globals = globals())
+    os.system("networksetup -setairportpower airport off")
+    os.system("networksetup -setairportpower airport on")
+    time.sleep(10)
+    t1 = subprocess.Popen(('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'), stdout=subprocess.PIPE)
+    global output 
+    output = str(subprocess.check_output(('awk', '/ BSSID/ {print substr($0, index($0, $2))}'), stdin=t1.stdout)).lower()
+    output = output.rstrip()
+    print(output)
+connect()
+
+while output != aim:
+    connect()
+
+
+if output == aim:
+    print('successfully connected to {}'.format(aim))
+else:
+    print('please restart, something bad happened')
+
+os.system('say "successfully connected"')
