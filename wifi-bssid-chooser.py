aa6eef5d4f67c0a4f384001eabad0ae76aa4dd93
gnzng <65827185+gnzng@users.noreply.github.com>
2020-08-24 08:57:11 +0200

    Update wifi-bssid-chooser.py

--- wifi-bssid-chooser.py
+++ wifi-bssid-chooser.py
@@ -1,7 +1,9 @@
 ### code by gnzng
-
 import objc, os, time, subprocess
 
+
+
+os.system("networksetup -setairportpower en0 on")
 process = subprocess.Popen(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport','-s'], stdout=subprocess.PIPE)
 out, err = process.communicate()
 process.wait()
@@ -12,22 +14,30 @@ print('make sure it is the only network your mac is able to connect to\n\n')
 aim =  str(raw_input('enter bssid to connect to in roaming network:\n')).lower()
 
 if aim == '':
-    aim =  'xx:xx:xx:xx:xx:xx'.lower()    #for often used connection
+    aim =  '00:00:00:00:00:00'.lower()    #for often used connection
     print('try to connect to {}'.format(aim))
+
+def get_bssid():
+    t1 = subprocess.Popen(('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'), stdout=subprocess.PIPE)
+    global output 
+    output = str(subprocess.check_output(('awk', '/ BSSID/ {print substr($0, index($0, $2))}'), stdin=t1.stdout)).lower()
+    output = output.rstrip()
 
 def connect():
     objc.loadBundle('CoreWLAN',
                     bundle_path = '/System/Library/Frameworks/CoreWLAN.framework',
                     module_globals = globals())
-    os.system("networksetup -setairportpower airport off")
-    os.system("networksetup -setairportpower airport on")
-    time.sleep(10) #can be optimized
-    t1 = subprocess.Popen(('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'), stdout=subprocess.PIPE)
-    global output 
-    output = str(subprocess.check_output(('awk', '/ BSSID/ {print substr($0, index($0, $2))}'), stdin=t1.stdout)).lower()
-    output = output.rstrip()
-    print(output)
-connect()
+    os.system("networksetup -setairportpower en0 off")
+    time.sleep(5)
+    os.system("networksetup -setairportpower en0 on")
+    time.sleep(5) 
+    get_bssid()
+    if output == '0:0:0:0:0:0':
+        time.sleep(30)    #the 5ghz takes longer to connect to in my experience
+        get_bssid()
+    print('connected to '+ output)
+
+get_bssid()
 
 while output != aim:
     connect()
@@ -35,6 +45,9 @@ while output != aim:
 
 if output == aim:
     print('successfully connected to {}'.format(aim))
-    os.system('say "successfully connected"')
 else:
     print('please restart, something bad happened')
+
+os.system('say "successfully connected"')
+
+
